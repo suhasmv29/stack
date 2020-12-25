f41355a533c02344fbeb406ad2333558708536a7
Suhas MV <suhas.mv@mountblue.tech>
2020-12-25 13:52:18 +0530

    Omniauth has been added

---
+++ app/controllers/stack_controller.rb
@@ -0,0 +1,38 @@
+class StackController < ApplicationController
+
+  def facebook
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def github
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def google_oauth2
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    flash[:error] = "There is a problem"
+    redirect_to new_user_registration_url
+  end
+
+end
