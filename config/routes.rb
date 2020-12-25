7a1e7c8ddc1c6386ee4155427f6f3c9986ae8896
Suhas MV <suhas.mv@mountblue.tech>
2020-12-26 01:37:45 +0530

    Html and css are updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   resources :questiontags
   resources :questions
   resources :answers
-  devise_for :users, controllers: { omniauth_callbacks: "stack"}
+  devise_for :users, controllers: { omniauth_callbacks: 'stack' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "questions#index"
+  #root 'home#index'
 end
